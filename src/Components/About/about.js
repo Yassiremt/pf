1074451c13e7138bfa1539a956a35a23cb7502c3
unknown <yassiremtioui@gmail.com>
2018-09-24 18:03:41 +0100

    update about component

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -3,14 +3,21 @@ import './about.css';
 import {Animated} from "react-animated-css";
 import {Container, Row, Col} from 'reactstrap';
 import myimg from '../../Images/mypic.jpg';
-import { FaInstagram, FaGithub, FaLinkedin,FaYoutube, FaTwitterSquare, FaEnvelope} from "react-icons/fa";
+import {
+  FaInstagram,
+  FaGithub,
+  FaLinkedin,
+  FaYoutube,
+  FaTwitterSquare,
+  FaEnvelope
+} from "react-icons/fa";
 const About = () => {
 
   return (<Animated animationIn="fadeInLeft" animationOut="fadeOut" isVisible={true}>
     <Container className="p-4 mt-4">
       <Row className="mt-3">
         <Col md="8" id="Sabt" className="p-4">
-          <h4>Hello People ..</h4>
+          <h4>Hello My Friends ..</h4>
           <p>My name is YASSIRE MTIOUI,
             <br/>
             I'm a freelance creative front‑end developer ,
@@ -40,12 +47,15 @@ const About = () => {
         </Col>
         <Col md="4" id="Simg" className="p-0">
           <img className="img-fluid" src={myimg} alt=""/>
-          <FaEnvelope className="icons mt-1 mx-1 em"/>
-          <FaInstagram className="icons mt-1 mx-1 ig"/>
-          <FaGithub className="icons mt-1 mx-1 gh"/>
-          <FaLinkedin className="icons mt-1 mx-1 li"/>
-          <FaYoutube className="icons mt-1 mx-1 yt"/>
-          <FaTwitterSquare className="icons mt-1 mx-1 tt"/>
+          <a href="mailto:yassiremtioui@gmail.com" className="em"><FaEnvelope className="icons mt-1 mx-1"/></a>
+
+          <FaInstagram className="icons mt-1 mx-1 ig dsbl"/>
+          <a href="https://github.com/Yassiremt" className="gh">
+            <FaGithub className="icons mt-1 mx-1"/></a>
+          <a href="https://www.linkedin.com/in/yassiremt/" className="li">
+            <FaLinkedin className="icons mt-1 mx-1"/></a>
+          <FaYoutube className="icons mt-1 mx-1 yt dsbl"/>
+          <FaTwitterSquare className="icons mt-1 mx-1 tt dsbl"/>
         </Col>
       </Row>
     </Container>
